eb6bf9301b656bb4c5cb4f55735b733300cef2a2
Daniel Sanchez Sanchez <1930412@upv.edu.mx>
2019-11-06 05:32:04 +0000

    Commit 0.0.2

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', '20948b6199d6e290a51d5be6995f6e85aacda56f21b9cd9a' );
 define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'DrkST(y(wEK?(0/c|%#F-CHJmLg69f/.jGFnsSO8Z-*L;.l{~5%Nxz[D;ec5-XRb' );
-define( 'SECURE_AUTH_KEY',  'J@l@a-+EXb~V{r(:s0erTk[@Num-@>)A0^+vQu};^hiFcwZ>qR=iA`C-T2:_$G&5' );
-define( 'LOGGED_IN_KEY',    'L@}9ZFS;%)&r5):$Lk*T.u.Wr3)Y<!BUlhBMqFAylQ&Xl+TWXutu;D!%{bz4q>D.' );
-define( 'NONCE_KEY',        'Vag*Rb3|fY)rny$9*[l,{`heqvirYZ.;N<EEkP<Ee^fCy+VuOX#3~;_h8yMJ*/W9' );
-define( 'AUTH_SALT',        'w+W=Mv4rT&&4/~k28b+v;2x^r}Y}E<.0E9e8zoq3m=.1dvK7@8MJ}Tg$D1p[,3sA' );
-define( 'SECURE_AUTH_SALT', '^u.HLrw} hwn,@M,!hk7:vUb#nEmc|&1=VOVqb]bVPtbY10 dY^!8l;V>qx?Q3,^' );
-define( 'LOGGED_IN_SALT',   '>d*[F%l,X+ST,7lCkWW7eQO^Xa;rUL,Y6FQ1>q85>F58rn?kHL,}Z9pI9WQ$2w](' );
-define( 'NONCE_SALT',       '&FUx5>I;vsKz,aFgm(:[{@= NV=}N<l*YemZnxd26#l@:l:-t->CZz1/uhX*6nZS' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 /**#@-*/
 
@@ -88,3 +88,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+define('FS_METHOD','direct');
